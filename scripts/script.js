c01d835f1ad874356ea4369259c3f58c76a4a267
HoagDuc <hduc599@gmail.com>
2022-06-03 22:52:15 +0700

    feat-1

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+var header = document.getElementById("header");
+var mobileMenu = document.getElementById("mobile-menu");
+var headerHeight = header.clientHeight;
+
+mobileMenu.onclick = function () {
+  var isClose = header.clientHeight === headerHeight;
+
+  if (isClose) {
+    header.style.height = "auto";
+  } else {
+    header.style.height = null;
+  }
+};
