bb63702a0640b65d6246969bd9457ea4671a5985
HoagDuc <hduc599@gmail.com>
2022-06-04 22:26:53 +0700

    add Register

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,12 @@
-var header = document.getElementById("header");
-var mobileMenu = document.getElementById("mobile-menu");
+var header = document.getElementById('header');
+var mobileMenu = document.getElementById('mobile-menu');
 var headerHeight = header.clientHeight;
 
 mobileMenu.onclick = function () {
   var isClose = header.clientHeight === headerHeight;
 
   if (isClose) {
-    header.style.height = "auto";
+    header.style.height = 'auto';
   } else {
     header.style.height = null;
   }
